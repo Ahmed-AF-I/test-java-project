2d99cc50e4319525ba82082646ffce6c30e70bf9
Ahmed-10 <ahmed.405xy@gmail.com>
2025-09-09 22:31:12 +0600

    The project completed

--- src/main/java/org/oop/Main.java
+++ src/main/java/org/oop/Main.java
@@ -1,7 +1,17 @@
 package org.oop;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("Test.txt");
+
+        String st = "This is a test for File handling in Java using FileWriter class.";
+        FileWriter fr = new FileWriter(file);
+        fr.write(st);
+        fr.flush();
+        fr.close();
     }
 }
