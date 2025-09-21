be7d53b7436df167c144b501900526b47b0c3849
Ahmed-10 <ahmed.405xy@gmail.com>
2025-09-22 01:14:50 +0600

    gatting improved by using BufferedReander & BufferdeWriter instd of FileReader & FileWriter

--- src/main/java/org/oop/Main.java
+++ src/main/java/org/oop/Main.java
@@ -1,29 +1,30 @@
 package org.oop;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("Test.txt");
 
-//        String st = "This is a test for File handling in Java using FileWriter class.";
-//        FileWriter fw = new FileWriter(file);
-//        fw.write(st);
-//        fw.flush();
-//        fw.close();
+//        BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(file, true));
+//        bw.newLine();
+//        bw.write("BufferedWriter Example");
+//        bw.newLine();
+//        bw.write("This is a test file.");
+//        bw.newLine();
+//        bw.write("This file is created using BufferedWriter.");
+//        bw.flush();
+//        bw.close();
 
-         FileReader fr = new FileReader(file);
-         char[] a = new char[(int)file.length()];
-         fr.read(a);
-
-         for (char c : a){
-            System.out.print(c);
-         }
-         fr.close();
-
+        BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append(System.lineSeparator());
+            System.out.println(line);
+        }
 
     }
 }
